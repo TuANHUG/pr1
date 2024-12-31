bd64925e37ca8700365e960f07c2a834ae7dc45b
Nguyen Hung <hungvh0375@gmail.com>
2024-12-31 22:04:42 +0700

    Updates

---
+++ precache-manifest.a1fb7168e36ab8e30b976fa4efc09842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78279813712bf8981a2096d8ba8ed5e",
+    "url": "/pr1/index.html"
+  },
+  {
+    "revision": "617e0257d5f43035dd0e",
+    "url": "/pr1/static/css/main.c664f7f6.chunk.css"
+  },
+  {
+    "revision": "2a1ceee91e55a61f7b7b",
+    "url": "/pr1/static/js/2.c91f30cc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pr1/static/js/2.c91f30cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617e0257d5f43035dd0e",
+    "url": "/pr1/static/js/main.3d20223a.chunk.js"
+  },
+  {
+    "revision": "b67fc2c372a482117304",
+    "url": "/pr1/static/js/runtime-main.10a4fc8a.js"
+  }
+]);
